13afc24d4f856be017add387aa85f6e14b998623
shubhamp28 <36756203+shubhamp28@users.noreply.github.com>
2018-02-25 00:41:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,6 +12,14 @@ app.get('/', function (req, res) {
 app.get('/article-one',function(req,res){
   res.sendFile(path.join(__dirname,'ui','article1.html'));
 });
+
+app.get('/article-two',function(req,res){
+  res.sendFile(path.join(__dirname,'ui','article2.html'));
+});
+
+app.get('/article-three',function(req,res){
+  res.sendFile(path.join(__dirname,'ui','article3.html'));
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
